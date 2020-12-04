d4f86db579adf45459c9d1cff6967caa00664755
kamral <kamral010101@gmail.com>
2020-12-05 00:44:47 +0300

    я добавил     password = serializers.CharField(write_only=True)

--- account/serializers.py
+++ account/serializers.py
@@ -2,11 +2,19 @@ from rest_framework import serializers
 from .models import User
 
 class UserRegistrationSerializers(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model=User
         fields=('username','first_name','last_name','age', 'password')
 
+    def create(self, validated_data):
+        user = super(UserRegistrationSerializers, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
 class UserLoginSerializers(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model=User
         fields=('username','first_name','last_name','password')
